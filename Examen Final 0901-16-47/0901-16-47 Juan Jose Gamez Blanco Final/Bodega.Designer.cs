3f8ac3b86c8d5abc28bbb8da6d6f2ae8562b10d9
JJGB2311 <jgamezb@miumg.edu.gt>
2020-06-03 12:15:45 -0600

    Mantenimientos Juan Jose Gamez 0901-16-47

---
+++ Examen Final 0901-16-47/0901-16-47 Juan Jose Gamez Blanco Final/Bodega.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace _0901_16_47_Juan_Jose_Gamez_Blanco_Final
+{
+    partial class Bodega
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.navegador1 = new CapaDeDiseno.Navegador();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.LblUsuario = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navegador1
+            // 
+            this.navegador1.BackColor = System.Drawing.Color.White;
+            this.navegador1.Location = new System.Drawing.Point(0, 1);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(850, 650);
+            this.navegador1.TabIndex = 0;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.BackColor = System.Drawing.SystemColors.Highlight;
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel,
+            this.LblUsuario});
+            this.statusStrip.Location = new System.Drawing.Point(0, 657);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip.Size = new System.Drawing.Size(849, 22);
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.Text = "StatusStrip";
+            this.statusStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip_ItemClicked);
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(62, 17);
+            this.toolStripStatusLabel.Text = "Usuario :";
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblUsuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(20, 17);
+            this.LblUsuario.Text = "...";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(0, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(850, 32);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // Bodega
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(849, 679);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.navegador1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Bodega";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bodega";
+            this.Load += new System.EventHandler(this.Mantenimiento1_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CapaDeDiseno.Navegador navegador1;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel LblUsuario;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ Examen Final 0901-16-47/0901-16-47 Juan Jose Gamez Blanco Final/vendedores.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0901_16_47_Juan_Jose_Gamez_Blanco_Final
+{
+    public partial class vendedores : Form
+    {
+        string usuario;
+        public vendedores(string user)
+        {
+            InitializeComponent();
+            usuario = user;
+            LblUsuario.Text = usuario;
+            string[] alias = { "No", "Nombre","Direccion","Telefono","Nit", "Estado" }; // Arreglo de nombres para campos
+            navegador1.asignarAlias(alias); // Asignar nombres
+            navegador1.asignarSalida(this); // Asignar form de salida
+            Color nuevoColor = System.Drawing.ColorTranslator.FromHtml("#ffffff"); // Deficion de 
+            navegador1.asignarColorFondo(nuevoColor);
+            navegador1.asignarColorFuente(Color.Blue);
+            navegador1.asignarAyuda("1"); // asignar 1 por defecto 
+
+            navegador1.asignarTabla("vendedores"); // tabla principal
+            navegador1.asignarNombreForm("Vendedores"); // Titulo y nombre del form
+        }
+
+        private void vendedores_Load(object sender, EventArgs e)
+        {
+            string aplicacionActiva = "1";
+            navegador1.ObtenerIdUsuario(usuario); // Pasa el parametro del usuario
+            navegador1.botonesYPermisosInicial(usuario, aplicacionActiva); // Consulta permisos al iniciar
+            navegador1.ObtenerIdAplicacion(aplicacionActiva);// Pasa el id de la aplicacion actual
+        }
+    }
+}
